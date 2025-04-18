6bb857c6c1485b07ce4ad7b8bbaa8625bfc4cb0f
ddoan <ddoan@innovqube.com>
2025-04-18 12:47:27 +0200

    Modify primitive of Computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -27,27 +27,20 @@ const Computers = ({ isMobile }) => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
-        position={[-20, 50, 10]}
+        position={[-10, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
-        shadow-mapSize={924}
+        shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.5 : 0.6}
-        position={isMobile ? [0, -2, -2] : [0, -3, -1.5]}
+        scale={isMobile ? 0.4 : 0.6}
+        position={isMobile ? [0, -0.5, -1.5] : [0, -3, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
-
-      {/*<primitive
-        object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -4.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />*/}
     </mesh>
   );
 };
